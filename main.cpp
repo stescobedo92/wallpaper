ca5b49fc4c049f2e23250f8a13b5fcb86fbcf66f
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-02-05 18:07:13 -0600

    Added the changeWallpaper method to change the desktop wallpaper according to the operating system

--- main.cpp
+++ main.cpp
@@ -7,6 +7,19 @@
     #include <cstdlib>
 #endif
 
+void changeWallpaper(const std::string& imagePath) {
+#ifdef _WIN32
+    // Change desktop background in Windows
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, (void*)imagePath.c_str(), SPIF_UPDATEINIFILE);
+    std::cout << "Desktop background changed in Windows." << std::endl;
+#else
+    // Change desktop background in Linux
+    std::string command = "gsettings set org.gnome.desktop.background picture-uri file://" + imagePath;
+    std::system(command.c_str());
+    std::cout << "Desktop background changed in Linux" << std::endl;
+#endif
+}
+
 int main()
 {
     std::cout<<"Hello world"<<std::endl;
